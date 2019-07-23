3d50fad59dea324145bbefdf986a98dfd8be0307
pwn4justice <20819473@qq.com>
2019-07-23 22:43:25 +0800

    'update'

--- wget.py
+++ wget.py
@@ -12,8 +12,11 @@ import time
 from urllib import parse
 
 def isWebsite(host):
-    pass
-
+    if not re.match("^([0-9]\.?)", host):
+        return True
+    else:
+        return False
+    
 
 def usage():
     print("usage: wget.py -u 'site' [options]")
@@ -36,12 +39,13 @@ def retrieveData(url, username=None, password=None):
         host = url.netloc.split(':')[0]
         port = int(url.netloc.split(':')[1])
 
-    if 'www' in host:
-        host = '.'.join(host.split('.')[1:])
+    #if 'www' in host:
+    #    host = '.'.join(host.split('.')[1:])
 
     if url.path != '':
-        resource = url.path.split('/')[-1:][0]
-
+        #resource = url.path.split('/')[-1:][0]
+        resource = url.path
+        
     # Print Info:
     print("Host: \t" + host)
     print("Port: \t" + str(port))
@@ -54,54 +58,85 @@ def retrieveData(url, username=None, password=None):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host,port))
 
+    #construct a request msg
+    msg = "GET " + resource + " HTTP/1.1\r\n"
+    
     if isWebsite(host):
-        pass
-
-    msg = "GET " + resource + " HTTP/1.1"
-    msg += "\r\n\r\n"
-
-    msg = bytes(msg, encoding='utf-8')
-    s.send(msg)
-    buffer = s.recv(1024)
-    print("[*] Header: \n" + buffer.decode().strip())
+        header_host = "Host: %s\r\n" % host
+        msg += header_host
         
+    msg += "\r\n\r\n"
+    print(msg)
+    msg = bytes(msg, encoding='utf-8')
+    
+    #send
+    s.send(msg)
+    buffer = s.recv(2048)
+    
+    print(buffer)
+    #split head and body
+    header = buffer.decode().split('\r\n\r\n')[0]
+    body = buffer.decode().split('\r\n\r\n')[1]
+    
+    print("[*] Header: \n" + header.strip())
+
     # get Content-Length:
-    content_length = re.findall("Content-Length:\s(.*?)\s\s", buffer.decode().strip(), re.M)[0]
-    content_length = int(content_length)
-    contentMB = content_length / 1024 / 1024
+    try:
+        content_length = re.findall("Content-Length:\s(.*?)\s\s", header.strip(), re.M)[0]
+        content_length = int(content_length)
+        contentMB = content_length / 1024 / 1024
+    except IndexError:
+        content_length = 0
+        contentMB = 0
     print("[*] Size: %.2f MB" % contentMB)
+    #print("[*] Body: %s \n" % body.strip())
 
     # record downloaded size
     size = 0
-    blocks = content_length/50
-    k = int( blocks - 1024 * ( blocks // 1024) )
-
+    
     ## time start 
     start = time.time()
+    body = bytes(body.encode('utf-8'))
+    
+                    
+    if resource == '/':
+        resource = "index.html"
+    else:
+        resource = resource.split('/')[-1]
+        
     while True:
-        buffer = s.recv(1024)
+        if len(body):
+            with open(resource, 'ab') as f:
+                f.write(body)
+            body = ""         
+        s.settimeout(5.0)
         
-        ## show download bar
-        size += len(buffer)
-        print('\r' + "[*] Downloading Process: %s%.2f%%" % ('>' * (size * 50 // content_length), size / content_length * 100), end='')
+        try:
+            buffer = s.recv(1024)
+        except socket.timeout:
+            buffer = b""
+            break                      
         
-        if not buffer:
-            break
-        if resource == '/':
-            resource = "index.html"
-
+        if not len(buffer):
+            break          
+        
         with open(resource, 'ab') as f:
             f.write(buffer)
-    
+
+        ## show download bar
+        try:
+            size += len(buffer)
+            print('\r' + "[*] Downloading Process: %s%.2f%%" % ('>' * (size * 50 // content_length), size / content_length * 100), end='')
+        except ZeroDivisionError:
+            print("No data was retrieved, shutdown ...")
+            sys.exit()
+            
     ## time end
     end = time.time()
     print("\n[*] Download Complete! ")
     print("[*] Cost: %.2fs ; Save to: %s - %.2f MB" % ( end-start, resource, contentMB))
     print("")
     s.close()
-
-    
-
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -140,9 +175,7 @@ if __name__ == "__main__":
 
     print('-' * 20)
     print("URL: \t" + url)
-    #if url is not an ip address, it will return 'bad request - 404'
-    #so when using this tool to wget a website, should constuct a more
-    #complicated request HEADER!!!
+
     if username and password:
         retrieveData(url, username=username, password=password)
     else:
